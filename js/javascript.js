939b0e7e24412d2b99933733315dfcb7bd29f6a0
Sergio Segura Blanco <ssegurab@gmail.com>
2022-03-08 11:57:53 +0100

    Aplicando cambios responsive

--- js/javascript.js
+++ js/javascript.js
@@ -1,19 +1,17 @@
-let text_lorem = "lorem ipsum dolor sit amet consectetur adipiscing elit Phasellus et felis nec leo efficitur vulputate Praesent id dui cursus imperdiet augue eget sagittis neque Suspendisse potenti Nullam eu purus eros Aliquam enim sem rhoncus non vulputate et convallis vitae leo Ut sed felis eu urna posuere facilisis quis vitae elit Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae Proin in pellentesque nisi Etiam faucibus ut dui in pharetra Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae Nunc euismod a massa sed laoreet Aenean fringilla augue euismod faucibus suscipit"
+let text_lorem = "lorem ipsum dolor sit amet consectetur adipiscing elit phasellus et felis nec leo efficitur vulputate praesent id dui cursus imperdiet augue eget sagittis neque suspendisse potenti nullam eu purus eros aliquam enim sem rhoncus non vulputate et convallis vitae leo ut sed felis eu urna posuere facilisis quis vitae elit vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae proin in pellentesque nisi etiam faucibus ut dui in pharetra vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae nunc euismod a massa sed laoreet aenean fringilla augue euismod faucibus suscipit"
 
-let lorem_print = ""; //això és el que s'imprimirà per pantalla
+let lorem_print = ""; //esto es lo que se imprimirá por pantalla
 
-//creem un array de paraules a partir del text original
-let array_lorem = text_lorem.split(" "); //separa el text en paraules a partir de l'espai
-
+//creamos un array de palabras a partir del texto original
+let array_lorem = text_lorem.split(" "); //separa el texto en palabras a partir del espacio
 console.log(array_lorem);
 
 
+function generate() {
+    let recogePalabras = document.getElementById("palabras__input").value;
+    let recogeParrafos = document.getElementById("parrafos__input").value;
 
-function captura() {
-    let recogePalabras = document.getElementById("palabras").value;
-    let recogeParrafos = document.getElementById("parrafos").value;
-
-    /* console.log(recogePalabras, recogeParrafos); */
+    console.log(recogePalabras, recogeParrafos);
 
     for (i=0; i < recogeParrafos; i++) {
         for (p=0; p < recogePalabras; p++) {
@@ -22,7 +20,7 @@ function captura() {
             lorem_print = lorem_print + array_lorem[aleatorio] + " ";
         }
 
-        document.getElementById("user_lorem").innerHTML += "<p> lorem ipsum " + lorem_print + ".</p>";
+        document.getElementById("paragraph__lorem").innerHTML += "<p> Lorem ipsum " + lorem_print + ".</p>" + "</br>";
 
         lorem_print = "";
 
